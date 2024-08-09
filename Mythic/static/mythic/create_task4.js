f6f5144d7b308ccb10767362ad135a8ee618eb92
talent <probablyjustwind@gmail.com>
2024-08-09 18:54:54 +0700

    Adjusted CSS for list items

--- Mythic/static/mythic/create_task4.js
+++ Mythic/static/mythic/create_task4.js
@@ -1,6 +1,7 @@
 const sortableList = document.querySelector(".sortable-list");
 const items = sortableList.querySelectorAll(".item");
 
+
 items.forEach(item => {
     item.addEventListener("dragstart", () => {
         // Adding dragging class to item after a delay
